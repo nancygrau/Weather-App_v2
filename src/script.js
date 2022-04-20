25eddf56e7a890940960362b8fd2ae4f6e9a1bbc
nancygrau <grau.nancy@gmail.com>
2022-04-20 11:04:29 -0400

    updated JS for sunrise and sunset time

--- src/script.js
+++ src/script.js
@@ -43,6 +43,18 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
 
 function showCityTemperature(response) {
   console.log(response);
@@ -56,8 +68,8 @@ function showCityTemperature(response) {
   temperatureElement.innerHTML = temperature;
   city.innerHTML = response.data.name;
   conditionElement.innerHTML = response.data.weather[0].description;
-  sunriseElement.innerHTML = response.data.sys.sunrise;
-  sunsetElement.innerHTML = response.data.sys.sunset;
+  sunriseElement.innerHTML = formatTime(response.data.sys.sunrise * 1000);
+  sunsetElement.innerHTML = formatTime(response.data.sys.sunset * 1000);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "8ee746d18f9f9f4609efcf4a58ee9252";
